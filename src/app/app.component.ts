b7e21670cb46fc8df3966564224484e588ecb6d3
Alejandro Burgos <alejandroburgos97@yahoo.com>
2021-10-27 15:51:56 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto007';
+  @ViewChild("selector1")
+  selector1!: SelectornumericoComponent;
+  @ViewChild("selector2")
+  selector2!: SelectornumericoComponent;
+
+  fijarSelector1(valor:number): void{
+    this.selector1.fijar(valor);
+  }
+  fijarSelector2(valor:number): void{
+    this.selector2.fijar(valor);
+  }
 }
